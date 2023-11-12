01f20cbb876f1ade297f7b3a22d088fda3e60297
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-12 18:07:48 +1100

    added inquirer prompts for user input when adding

--- index.js
+++ index.js
@@ -25,7 +25,65 @@ const landingPage = `
     // You might also want to include a seeds.sql file to pre-populate your database, making the development of individual features much easier.
 
 // Add a department > Prompted to enter the name of the department , which is then added to the database
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'department-name',
+        message: 'Please enter the name of the department.',
+    },
+])
+.then(() => {
+
+});
 // Add a role > Prompted to enter info, added to database
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'role-name',
+        message: 'Please enter the title of the role.',
+    },
+    {
+        type: 'input',
+        name: 'role-salary',
+        message: 'Please enter the salary of the role.',
+    },
+    {
+        type: 'input',
+        name: 'role-department',
+        message: 'Please enter the department the role belongs to.',
+    },
+])
+.then(() => {
+
+});
 // Add an employee > Prompted to enter info, added to database
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'employee-name',
+        message: "Please enter the employee's first name.",
+    },
+    {
+        type: 'input',
+        name: 'employee-surname',
+        message: "Please enter the employee's last name.";
+    },
+    {
+        type: 'input',
+        name: 'employee-role',
+        message: "Please enter the employee's role.",
+    },
+    {
+        type: 'input',
+        name: 'employee-manager',
+        message: "Please enter the name of the employee's manager."
+    },
+])
+.then(() => {
+
+});
 
 // Update an employee role > Prompted to select an employee to update & input new role , added to database
