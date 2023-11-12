c42f72bc13af9c5613ccbfa4b32d965f7db1f703
Aleesha Naxakis <aleesha-@hotmail.com>
2023-11-12 20:04:53 +1100

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,30 @@
+-- Drop the tables if they exist
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+-- Create the "department" table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Create the "role" table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Create the "employee" table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
